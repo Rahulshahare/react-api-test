ae874e0522e056df5ff7a2ab9b94e902febd9091
Rahul Shahare <rahulshahare@gmail.com>
2020-01-10 15:06:12 +0530

    initial upload

---
+++ precache-manifest.e2694bcf5eb4fbcd4ad7594118cf0f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580841b02e9cfd599f0682461b536446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80a4b740041ad92a0d7",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d8941dedbc34f8cfabfa",
+    "url": "/static/css/main.9e93e030.chunk.css"
+  },
+  {
+    "revision": "a80a4b740041ad92a0d7",
+    "url": "/static/js/2.673781d7.chunk.js"
+  },
+  {
+    "revision": "a70924e079ee0f48d959cf4266326980",
+    "url": "/static/js/2.673781d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8941dedbc34f8cfabfa",
+    "url": "/static/js/main.d8d402bf.chunk.js"
+  },
+  {
+    "revision": "7101ac549797742b06ab",
+    "url": "/static/js/runtime-main.509a42f9.js"
+  }
+]);
